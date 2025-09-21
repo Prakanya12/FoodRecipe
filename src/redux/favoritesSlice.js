0748fb994aaf1ba87724a219fc22c3f16356da3f
Prakanya12 <prakanya1207@gmail.com>
2025-09-21 18:22:22 +0530

    Update favoritesSlice.js

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -8,6 +8,17 @@ const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
+     toggleFavorite: (state, action) => {
+      const recipe = action.payload; // Expecting the entire article object
+      const existingIndex = state.favoriteRecipes.findIndex(
+        (item) => item.id === recipe.idRecipe // Assuming articles have an 'id' property
+      );
+      if (existingIndex >= 0) {
+        state.favoriteRecipes.splice(existingIndex, 1); // Remove from favorites
+      } else {
+        state.favoriteRecipes.push(article); // Add to favorites
+      }
+    },
    
   },
 });
